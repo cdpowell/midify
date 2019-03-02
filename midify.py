5bd022cd4568ba779d7f5ec3a27334ed7fec4e60
cdpo224UKY <31912114+cdpo224UKY@users.noreply.github.com>
2019-03-02 15:34:23 -0500

    Lines

--- midify.py
+++ midify.py
@@ -9,12 +9,29 @@ if __name__ == '__main__':
     img_size = img.shape[:2]
 
     img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    thresh, img_bw = cv2.threshold(img_gray, 128, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
+    thresh = 150
+    img_bw = cv2.threshold(img_gray, thresh, 255, cv2.THRESH_BINARY)[1]
 
     edges = cv2.Canny(img_bw, 50, 150, apertureSize=3)
-    lines = cv2.HoughLinesP(edges, 1, np.pi / 180, 150, None, 0, 0)
-    for line in lines:
+    p_lines = cv2.HoughLinesP(edges, 1, np.pi / 180, 150, 100, 0, 0)
+    # circles = cv2.HoughCircles(img_bw, cv2.HOUGH_GRADIENT, 1, 20, param1=100, param2=100, minRadius=0, maxRadius=0)
+    # circles = np.uint16(np.around(circles))
+
+    for line in p_lines:
         for x1, y1, x2, y2 in line:
             cv2.line(img, (x1, y1), (x2, y2), (0, 0, 255), 2)
+    # circles = np.uint16(np.around(circles))
+    # print(circles)
+    # for i in circles[0, :]:
+    #     # draw the outer circle
+    #     cv2.circle(img, (i[0], i[1]), i[2], (0, 255, 0), 2)
+    #     # draw the center of the circle
+    #     cv2.circle(img, (i[0], i[1]), 2, (0, 0, 255), 3)
 
-    cv2.imwrite('data/images/score_2_lines.jpg', img)
+    # lines = cv2.HoughLines(edges, 1, np.pi / 180, 150, None, 0, 0)
+    # for line in p_lines:
+    #     for x1, y1, x2, y2 in line:
+    #         cv2.line(img, (x1, y1), (x2, y2), (0, 255, 0), 2)
+
+    cv2.imwrite('data/images/score_3_bw.jpg', img_bw)
+    cv2.imwrite('data/images/score_3_lines.jpg', img)
