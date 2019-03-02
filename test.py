76e9ff834b6ecc2a45d5651d4223011de77d468b
Kevin Zhang <kzh227@g.uky.edu>
2019-03-02 15:53:51 -0500

    file with functions
    has staffLines() and writeMIDI()

---
+++ test.py
@@ -0,0 +1,44 @@
+import cv2
+from midiutil import MIDIFile
+
+THRESHHOLD = 3
+
+def staffLines(plines):
+	plines = sorted(plines, key=lambda x: x[1])
+	lines = []
+	temp = [0]
+	prevY = -THRESHHOLD - 1
+	for coords in plines:
+		if coords[1] > prevY + THRESHHOLD:
+			avg = reduce(lambda x, y: x + y, temp), len(temp)
+			lines.append(avg)
+			temp = []
+			prevY = coords[1]
+		temp.append(coords[1])
+	avg = reduce(lambda x, y: x + y, temp), len(temp)
+	lines.append(avg)
+	lines.pop(0)
+	return lines
+
+def writeMIDI(fileName, notes):
+	
+	degrees  = [60, 62, 64, 65, 67, 69, 71, 72]  # MIDI note number
+	track    = 0
+	channel  = 0
+	time     = 0    # In beats
+	duration = 1    # In beats
+	tempo    = 60   # In BPM
+	volume   = 100  # 0-127, as per the MIDI standard
+	
+	MyMIDI = MIDIFile(1)  # One track, defaults to format 1 (tempo track is created
+						# automatically)
+	MyMIDI.addTempo(track, time, tempo)
+	
+	# adds each note to the track
+	for note in notes:
+		MyMIDI.addNote(track, channel, pitch, time, note[0], volume)
+		time += note[1]
+	
+	# writes to the filename
+	with open(fileName, "wb") as output_file:
+		MyMIDI.writeFile(output_file)
